160b45ad7338fe4ff5c618e665f2a3c1e6d5e046
Abbygail Pollack <abbypollack00@gmail.com>
2023-11-22 12:14:59 -0500

    add user route for local authentication

---
+++ routes/user.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+const knex = require('../knexfile');
+
+const JWT_SECRET = process.env.SESSION_SECRET;
+
+// Register user
+router.post('/register', async (req, res) => {
+    const { email, password, ...otherData } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email and password are required." });
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await knex('users').insert({ ...otherData, email, password: hashedPassword });
+        res.status(201).json({ message: "User created successfully." });
+    } catch (error) {
+        res.status(500).json({ message: "Error registering user." });
+    }
+});
+
+// User login
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await knex('users').where({ email }).first();
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: '24h' });
+            res.json({ token });
+        } else {
+            res.status(401).json({ message: "Invalid credentials." });
+        }
+    } catch (error) {
+        res.status(500).json({ message: "Error logging in." });
+    }
+});
+
+//  Current user
+router.get('/current', (req, res) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ message: "No token provided." });
+    }
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        const userId = decoded.userId;
+        knex('users').where({ id: userId }).first().then(user => {
+            res.json(user);
+        });
+    } catch (error) {
+        res.status(401).json({ message: "Invalid token." });
+    }
+});
+
+module.exports = router;
