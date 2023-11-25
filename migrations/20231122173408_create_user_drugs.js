aa5d3eab67630772f99593598d4376ea713871e5
Abbygail Pollack <abbypollack00@gmail.com>
2023-11-25 11:05:29 -0500

    added strength column to user_drugs table

--- migrations/20231122173408_create_user_drugs.js
+++ migrations/20231122173408_create_user_drugs.js
@@ -1,13 +1,14 @@
 exports.up = function(knex) {
-    return knex.schema.createTable('user_drugs', table => {
+  return knex.schema.createTable('user_drugs', table => {
       table.increments('id').primary();
       table.integer('user_id').unsigned().notNullable();
       table.foreign('user_id').references('users.id');
       table.string('drug_name').notNullable();
-      table.string('rxnorm_id'); 
+      table.string('rxnorm_id');
+      table.string('strength');
       table.timestamps(true, true);
-    });
-  };
+  });
+};
   
   exports.down = function(knex) {
     return knex.schema.dropTable('user_drugs');
