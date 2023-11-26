6287388dfff1caf2eab7a26bebc060d8cddef307
Abbygail Pollack <abbypollack00@gmail.com>
2023-11-26 14:34:49 -0500

    moved the get endpoint for fetching and filtering user drugs based on specific reminder time to medications.js routing file

--- reminderService.js
+++ reminderService.js
@@ -19,20 +19,3 @@ cron.schedule('* * * * *', async () => {
     console.error('Error sending out reminders:', error);
   }
 });
-
-router.get('/reminders', authorize, async (req, res) => {
-  const { time } = req.query;
-  const { userId } = req.user;
-
-  try {
-    const userDrugs = await db('user_drugs')
-      .where({ user_id: userId })
-      .andWhereRaw('JSON_CONTAINS(reminder_times, ?)', [`"${time}"`])
-      .select('id', 'drug_name', 'strength', 'reminder_times');
-
-    res.json({ reminders: userDrugs.filter(drug => drug.reminder_times.includes(time)) });
-  } catch (error) {
-    console.error('Error fetching reminders:', error);
-    res.status(500).json({ message: "Error fetching reminders." });
-  }
-});
