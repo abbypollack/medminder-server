9c51f1e7289e246a515964bc4a0a9ea752a2b24a
Abbygail Pollack <abbypollack00@gmail.com>
2023-11-28 16:00:51 -0500

    fixed logged medications staying on MyMedications component

--- reminderService.js
+++ reminderService.js
@@ -3,6 +3,9 @@ const cron = require('node-cron');
 const axios = require('axios');
 const moment = require('moment');
 const authorize = require('./middleware/authorize');
+const twilio = require('twilio');
+const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
 
 cron.schedule('* * * * *', async () => {
   console.log('Running a task every minute');
@@ -14,8 +17,15 @@ cron.schedule('* * * * *', async () => {
     const reminders = response.data;
 
     reminders.forEach(reminder => {
-      console.log(`Reminder for ${reminder.drug_name}: Take your medication.`);   // TODO: Integrate with Twilio or Nodemailer (free alternative)
+      client.messages.create({
+        body: `Reminder for ${reminder.drug_name}: Take your medication.`,
+        to: phone,
+        from: process.env.TWILIO_PHONE_NUMBER
+      })
+      .then(message => console.log(message.sid))
+      .catch(error => console.error(error));
     });
+    
 
   } catch (error) {
     console.error('Error sending out reminders:', error);
@@ -30,13 +40,21 @@ router.get('/reminders', authorize, async (req, res) => {
 
   try {
     const userDrugs = await db('user_drugs')
+      .join('users', 'users.id', 'user_drugs.user_id')
       .where({ user_id: userId })
       .andWhereRaw('JSON_CONTAINS(reminder_times, ?)', [`"${time}"`])
-      .select('id', 'drug_name', 'strength', 'reminder_times');
+      .select('user_drugs.id', 'user_drugs.drug_name', 'user_drugs.strength', 'user_drugs.reminder_times', 'users.phone');
 
-    res.json({ reminders: userDrugs.filter(drug => drug.reminder_times.includes(time)) });
+    const remindersWithPhone = userDrugs.filter(drug => drug.reminder_times.includes(time))
+      .map(drug => ({
+        drug_name: drug.drug_name,
+        phone: drug.phone
+      }));
+
+    res.json({ reminders: remindersWithPhone });
   } catch (error) {
     console.error('Error fetching reminders:', error);
     res.status(500).json({ message: "Error fetching reminders." });
   }
 });
+
